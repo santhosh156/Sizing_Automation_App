4b87b89b3fc12ebf72109cb7f43ee7ac6ad96cc1
santhosh <santhosh156@gmail.com>
2018-07-20 12:55:00 +0530

    Initial version of Sizing Automation App

---
+++ project/views.py
@@ -0,0 +1,15 @@
+from .import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html')
